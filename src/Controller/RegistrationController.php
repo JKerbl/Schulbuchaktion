b912b70e061bd3046a283a0f8ff787e10a280508
DavidStefanRestea <drestea@htl-steyr.ac.at>
2024-03-14 11:16:31 +0100

    Implemented Login Function

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/registration', name: 'registration')]
+    public function index(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $regform = $this->createFormBuilder()
+            ->add('username', TextType::class, ['label' => 'Username'])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'required' => true,
+                'first_options' => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password']
+            ])
+            ->add('register', SubmitType::class)
+            ->getForm();
+
+        $regform->handleRequest($request);
+
+        if ($regform->isSubmitted() && $regform->isValid()) {
+            $input = $regform->getData();
+            $user = new User;
+            $user->setUsername($input['username']);
+            $user->setPassword($passwordHasher->hashPassword($user, $input['password']));
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirect($this->generateUrl('home'));
+        }
+
+        return $this->render('registration/index.html.twig', [
+            'regform' => $regform->createView()
+        ]);
+    }
+}
--- src/Security/CustomAuthenticator.php
+++ src/Security/CustomAuthenticator.php
@@ -49,8 +49,8 @@ class CustomAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('home'));
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
