95938b9c9f2f940ccc7a91f8b5e07f3748b0163c
JKerbl <117641110+JKerbl@users.noreply.github.com>
2024-04-08 08:33:42 +0200

    Updating the path to home controller to set the landing page to the default page after logout + small changes in the README.md

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'home')]
+    #[Route('/home', name: 'home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
