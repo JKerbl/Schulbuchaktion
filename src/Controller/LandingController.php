4ef047950b792993a0d02b1d0a5dc18728b3f976
JKerbl <117641110+JKerbl@users.noreply.github.com>
2024-04-08 08:32:30 +0200

    Creating a LandingController to make a nice and catchy website for the user if he/she visits our website and designing it with css and basic structure with twig

---
+++ src/Controller/LandingController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class LandingController extends AbstractController
+{
+    #[Route('/', name: 'landing_page')]
+    public function index(): Response
+    {
+
+
+        return $this->render('landing/index.html.twig', [
+            'controller_name' => 'LandingController',
+        ]);
+    }
+}
