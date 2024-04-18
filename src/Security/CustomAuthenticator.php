7211a664cd80273ed1d97d92b3da73887bbd0b6c
JKerbl <117641110+JKerbl@users.noreply.github.com>
2024-04-18 14:34:51 +0200

    Updated the paths, defined on which pages the user can go without login or with loging, some changes to the picture-sizing on the landing page

--- src/Security/CustomAuthenticator.php
+++ src/Security/CustomAuthenticator.php
@@ -49,7 +49,7 @@ class CustomAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('home'));
+        return new RedirectResponse($this->urlGenerator->generate('landing_page'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
