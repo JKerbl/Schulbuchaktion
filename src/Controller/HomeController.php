a76c36c58b1e6c0bc457886059fa00395758e3a1
SKaramanGit <skaraman@htl-steyr.ac.at>
2024-04-25 10:21:50 +0200

    Implemented basic search function in HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
@@ -20,6 +21,28 @@ class HomeController extends AbstractController
   
         return $this->render('home/index.html.twig', [
             'results' => $results,
+        ]);
+    }
+
+    #[Route('/search', name: 'search')]
+    public function search(Request $request): Response {
+        $query = $request->get('query', '');
+
+        $results = array(
+            "Ingenieur-Mathematik 4.-5. Klasse, HTL",
+            "Blattwerk Deutsch - Texte III-V, 3.-4. Klasse, HTL",
+            "Best Shots, 3.-5. Klasse, AHS",
+            "Recht für Techniker, 4. Klasse, HTL",
+            "Z",
+            "FKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKSFKS");
+
+        $filteredResults = array_filter($results, function($item) use ($query) {
+            return strpos($item, $query) !== false;
+        });
+
+        $user = $this->getUser();
+        return $this->render('home/index.html.twig', [
+            'results' => $filteredResults, 'user' => $user
         ]);
     }
 }
