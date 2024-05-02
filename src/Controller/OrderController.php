aabe0ef981706749f7c8be5481184e4943e9d703
SKaramanGit <skaraman@htl-steyr.ac.at>
2024-05-02 14:59:15 +0200

    Combination of order logic and order design

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,18 +2,39 @@
 
 namespace App\Controller;
 
+use App\Repository\BookRepository;
+use App\Repository\SchoolClassRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use function PHPUnit\Framework\isEmpty;
 
-class OrderController extends AbstractController
-{
-    #[Route('/order', name: 'order')]
-    public function index(): Response {
+class OrderController extends AbstractController {
+    #[Route('/order/{id}', name: 'order')]
+    public function index($id, BookRepository $br, SchoolClassRepository $scr): Response {
+        $res = array();
+        $books = $br->findAll();
+        foreach ($books as $key => $book) {
+            $res[] = array(
+                'id' => $book->getId(),
+                'shortTitle' => $book->getShortTitle()
+            );
+        }
         $user = $this->getUser();
 
+        $book = $br->find($id);
+        $classes = $scr->findAll();
+
+        /*if ($classes === []) {
+            return $this->render('home/index.html.twig', [
+                'results' => $res, 'user' => $user,
+                'searchInput' => ""
+            ]);
+        }*/
+
         return $this->render('order/index.html.twig', [
-            'user' => $user,
+            'user' => $user, 'book' => $book,
+            'classes' => $classes,
         ]);
     }
 }
