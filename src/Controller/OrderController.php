3be23be077d697104fb24833032856cbcbba206c
SKaramanGit <skaraman@htl-steyr.ac.at>
2024-06-06 10:09:33 +0200

    Implemented function to automatically fetch classes for ordering

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\BookRepository;
 use App\Repository\SchoolClassRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -24,12 +25,12 @@ class OrderController extends AbstractController {
         $book = $br->find($id);
         $classes = $scr->findAll();
 
-        /*if ($classes === []) {
+        if ($classes === []) {
             return $this->render('home/index.html.twig', [
                 'results' => $res, 'user' => $user,
                 'searchInput' => ""
             ]);
-        }*/
+        }
 
         return $this->render('order/index.html.twig', [
             'user' => $user, 'book' => $book,
@@ -37,6 +38,25 @@ class OrderController extends AbstractController {
         ]);
     }
 
+    #[Route('/get-class-data/{id}', name: 'get_class_data')]
+    public function getClassData($id, SchoolClassRepository $scr) {
+        $class = $scr->find($id);
+
+        if (!$class) {
+            throw $this->createNotFoundException(
+                'No class found for id ' . $id
+            );
+        }
+
+        $data = [
+            'studentAmount' => $class->getStudentsAmount(),
+            'repeaterAmount' => $class->getRepAmount(),
+            'usedBudgetAmount' => $class->getUsedBudget(),
+            'budgetAmount' => $class->getBudget()
+        ];
+
+        return new JsonResponse($data);
+    }
 
     #[Route('/orders', name: 'app_orders')]
     public function showOrders()
