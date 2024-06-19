54b1b1e43e0adfb73311e67d72ed1f74a12baf03
Felix Gärtner <fgaertne@htl-steyr.ac.at>
2024-06-19 20:56:54 +0200

    Booklist created

---
+++ src/Controller/BookController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Repository\BookRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/book')]
+class BookController extends AbstractController
+{
+    #[Route('/', name: 'app_book_index', methods: ['GET'])]
+    public function index(BookRepository $bookRepository): Response
+    {
+        return $this->render('book/index.html.twig', [
+            'books' => $bookRepository->findAll(),
+        ]);
+    }
+}
