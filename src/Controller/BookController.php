bba0f238cba2186e540c69fe1f0bfff3d8d3778e
SKaramanGit <skaraman@htl-steyr.ac.at>
2024-06-19 22:35:24 +0200

    Controller-Logic implementation of the pagination

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -6,16 +6,40 @@ namespace App\Controller;
 use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/book')]
+#[Route('/books')]
 class BookController extends AbstractController
 {
-    #[Route('/', name: 'app_book_index', methods: ['GET'])]
-    public function index(BookRepository $bookRepository): Response
+    public function __construct(private int $limit) {}
+
+    private function getMaxPages(BookRepository $bookRepository, int $limit, string $search = null): int
     {
+        $totalEntries = $bookRepository->getTotalEntries($search);
+        return ceil($totalEntries / $limit);
+    }
+
+    private function getCurrentPage(Request $request, int $maxPages): int
+    {
+        $page = (int) $request->get('page', 1);
+        return min(max($page, 1), $maxPages);
+    }
+
+    #[Route('/', name: 'app_book_index', methods: ['GET'])]
+    public function index(BookRepository $bookRepository, Request $request): Response
+    {
+        $search = $request->query->get('search', '');
+        $maxPages = $this->getMaxPages($bookRepository, $this->limit, $search);
+        $currentPage = $this->getCurrentPage($request, $maxPages);
+
+        $books = $bookRepository->getPaginatedEntries($this->limit, $currentPage, $search);
+
         return $this->render('book/index.html.twig', [
-            'books' => $bookRepository->findAll(),
+            'books' => $books,
+            'maxPages' => $maxPages,
+            'currentPage' => $currentPage,
+            'search' => $search,
         ]);
     }
 }
