243387b1f5746d118cd1f5500c66c463e23200cf
arechber <arechber@htl-steyr.ac.at>
2025-01-31 09:02:51 +0100

    Changed the registration so you can no longer register yourself as an Admin

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -30,9 +30,6 @@ class RegistrationController extends AbstractController
             ->add('roles', ChoiceType::class, [
                 'choices' => [
                     'User' => 'ROLE_USER',
-                    'AV' => 'ROLE_AV',
-                    'FV' => 'ROLE_FV',
-                    'Admin' => 'ROLE_ADMIN',
                 ],
                 'label' => 'Role',
                 'multiple' => false,
